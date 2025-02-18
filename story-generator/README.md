b1d87d4155520b426b529d7ee1eb8834594328b2
Sowon Kim <sso626262@naver.com>
2025-02-18 20:07:26 +0900

    feat: initial project setting

---
+++ story-generator/README.md
@@ -0,0 +1,5 @@
+# lyrics2story generator web
+
+## 참고자료
+
+https://19-97.tistory.com/167https://19-97.tistory.com/167
---
+++ story-generator/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="text-blue-300 text-3xl">test😃</div>
+    </>
+  )
+}
+
+export default App
